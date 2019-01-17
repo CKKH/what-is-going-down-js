1a77112facb94932dfa2d78f1419a86435676c0c
CKKH <chriskkhassan@gmail.com>
2019-01-17 20:45:11 +0000

    Unit test 8 failing: Message method userInput not defined

--- spec/MessageSpec.js
+++ spec/MessageSpec.js
@@ -1,10 +1,17 @@
-'use strict';
+/* global describe, it, expect, beforeEach */
+
+'use strict'
 
 describe("Message", () => {
 
   it("instance of Message can be stored as a variable", () => {
-    var message = new Message;
+    var message = new Message();
     expect(message instanceof Message).toBe(true);
+  });
+
+  it("should respond to the method .userInput", () => {
+    var message = new Message();
+    expect(message.userInput).toBeDefined();
   });
 
 });
