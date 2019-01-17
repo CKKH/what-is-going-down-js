fde02530ce3de43b536659baf5f463d0492321be
CKKH <chriskkhassan@gmail.com>
2019-01-17 21:37:34 +0000

    Refactored Message tests with before each loop

--- spec/MessageSpec.js
+++ spec/MessageSpec.js
@@ -3,14 +3,17 @@
 'use strict'
 
 describe("Message", () => {
+  let message;
+
+  beforeEach( () => {
+    message = new Message("Hello, Chris");
+  });
 
   it("instance of Message can be stored as a variable", () => {
-    var message = new Message();
     expect(message instanceof Message).toBe(true);
   });
 
   it("should return message when .userInput called", () => {
-    var message = new Message("Hello, Chris");
     expect(message.userInput).toEqual("Hello, Chris");
   });
 
