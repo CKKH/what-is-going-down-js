43a27b5dd1f2f737001e6d093ae7a2617b091f98
CKKH <chriskkhassan@gmail.com>
2019-01-17 20:14:48 +0000

    Refactored MessageBoard tests with before each loop

--- spec/MessageBoardSpec.js
+++ spec/MessageBoardSpec.js
@@ -3,20 +3,23 @@
 'use strict'
 
 describe("MessageBoard", () => {
+  let messageBoard;
+  let message;
+
+  beforeEach(function() {
+    messageBoard = new MessageBoard();
+    message = jasmine.createSpy('message')
+  });
 
   it("instance of MessageBoard can be stored as a variable", () => {
-    var messageBoard = new MessageBoard();
     expect(messageBoard instanceof MessageBoard).toBe(true);
   });
 
   it("returns an array when .messageList called", () => {
-    var messageBoard = new MessageBoard();
     expect(messageBoard.messageList).toEqual([]);
   });
 
   it("responds to the method .save(message)", () => {
-    var messageBoard = new MessageBoard();
-    var message = new Message();
     expect(messageBoard.save(message)).toBeDefined();
   });
 
