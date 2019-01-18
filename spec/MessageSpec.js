f17e569fe338cdd26470a20b085e07264115ebc4
CKKH <chriskkhassan@gmail.com>
2019-01-18 06:30:07 +0000

    Unit test 11 failing: .timeStamp does not return time message was created

--- spec/MessageSpec.js
+++ spec/MessageSpec.js
@@ -4,21 +4,27 @@
 
 describe("Message", () => {
   let message;
+  let time;
 
   beforeEach( () => {
-    message = new Message("Hello, Chris");
+    time = new Date
+    message = new Message("Hello, Chris", time);
   });
 
   it("instance of Message can be stored as a variable", () => {
     expect(message instanceof Message).toBe(true);
   });
 
-  it("should return message when .userInput called", () => {
+  it(".userInput returns message content", () => {
     expect(message.userInput).toEqual("Hello, Chris");
   });
 
   it("should respond to the method .timeStamp", () => {
     expect(message.timeStamp).toBeDefined();
+  });
+
+  it(".timeStamp returns time message was created", () => {
+    expect(message.timeStamp).toEqual(time);
   });
 
 });
